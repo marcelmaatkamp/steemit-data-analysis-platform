e305b35b95157fc37364ab064e72e382472446b0
marcel <m.maatkamp@gmail.com>
2018-01-14 08:54:41 +0100

    convenience methods added for steemdata

--- src/main/java/application/controller/VoteController.java
+++ src/main/java/application/controller/VoteController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Date;
+import java.time.*;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
@@ -17,13 +21,26 @@ public class VoteController {
     @Autowired
     MongoService mongoService;
 
-    private static final String template = "Hello, %s!";
+    @RequestMapping("/api/steemdata/vote/today")
+    public List<application.model.mongodb.AccountOperations.Vote> steemDataVotesToday(@RequestParam(value="name", defaultValue="World") String name) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startOfToday = now.with(LocalTime.MIDNIGHT);
+        return mongoService.getVotesBetween(startOfToday,now);
+    }
 
-    @RequestMapping("/api/vote/today")
-    public List<application.model.mongodb.AccountOperations.Vote> greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        Calendar now = GregorianCalendar.getInstance();
-        Calendar yesterday = GregorianCalendar.getInstance();
-        yesterday.add(Calendar.DAY_OF_MONTH, -1);
+    @RequestMapping("/api/steemdata/vote/last24h")
+    public List<application.model.mongodb.AccountOperations.Vote> steemDataVotesLast24h(@RequestParam(value="name", defaultValue="World") String name) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime yesterday = now.minus(1, ChronoUnit.DAYS);
         return mongoService.getVotesBetween(yesterday,now);
     }
+
+
+    @RequestMapping("/api/vote/steemdata/yesterday")
+    public List<application.model.mongodb.AccountOperations.Vote> steemDataVotesYesterday(@RequestParam(value="name", defaultValue="World") String name) {
+        LocalDateTime dayBeforeYesterday = LocalDate.now().minusDays(2).atStartOfDay();
+        LocalDateTime yesterday = LocalDate.now().minusDays(1).atStartOfDay();
+        return mongoService.getVotesBetween(dayBeforeYesterday, yesterday);
+    }
+
 }
--- src/main/java/application/service/MongoService.java
+++ src/main/java/application/service/MongoService.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.List;
 
@@ -29,6 +32,19 @@ public class MongoService {
                         and("timestamp").
                                 gte(start.getTime()).
                                 lt(end.getTime())).
+                        with(new Sort("timestamp", "-1")).
+                        limit(10), Vote.class);
+        return votes;
+    }
+
+    public List<Vote> getVotesBetween(LocalDateTime dayBeforeYesterday, LocalDateTime yesterday) {
+        // yesterday.atZone(ZoneId.systemDefault()
+        List<Vote> votes =
+                mongoTemplate.find(query(
+                        where("type").is("vote").
+                                and("timestamp").
+                                gte(dayBeforeYesterday).
+                                lt(yesterday)).
                         with(new Sort("timestamp", "-1")).
                         limit(10), Vote.class);
         return votes;
