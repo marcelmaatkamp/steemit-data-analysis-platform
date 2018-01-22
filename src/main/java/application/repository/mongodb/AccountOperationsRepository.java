6507cc08525e5cbf1a65fff4c904e4c63faa312b
marcel <m.maatkamp@gmail.com>
2018-01-22 19:04:25 +0100

    new version with pagable

--- src/main/java/application/repository/mongodb/AccountOperationsRepository.java
+++ src/main/java/application/repository/mongodb/AccountOperationsRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Calendar;
+
 public interface AccountOperationsRepository extends MongoRepository<Vote, String> {
     Vote findByVoter(String name);
 
-    @Query(value = "{'type': 'vote'}")
+    @Query("{'type': 'vote'}")
     Page<Vote> listVotes(Pageable pageable);
+
+    @Query("{'type': 'vote', 'timestamp': ?0 }")
+    Page<Vote> listVotesOnDate(Pageable pageable, Calendar calendar);
 
 }
